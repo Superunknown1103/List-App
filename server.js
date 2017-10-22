53d9947853740c2ed2d6ba8929cf454ecaa84c90
Superunknown1103 <superunknown1103@gmail.com>
2017-10-22 08:50:52 -0500

    postgres debugging

--- server.js
+++ server.js
@@ -6,7 +6,23 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-// Sets up the Express App
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+//Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
